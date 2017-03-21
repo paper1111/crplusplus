3ae7b5b5151fc2ed500a52e0f1610e29b6e44c9e
paper1111 <hilarylau119@gmail.com>
2017-03-22 07:45:28 +0800

    Add splash screen

--- installer.sh
+++ installer.sh
@@ -8,3 +8,13 @@ boldblue=${bold}$(tput setaf 4) #  blue
 boldwhite=${bold}$(tput setaf 7) #  white
 boldgreen=${bold}$(tput setaf 2) # green
 reset=$(tput sgr0)             # Reset
+
+echo "${boldgreen}Welcome to the Compiler++ installer!${reset}"
+
+${bold}
+echo "  CCCCCC  RRRRR        +           +"
+echo " CC       R    R       +           +"
+echo "CC        RRRRR    +++++++++   +++++++++"
+echo " CC       R    R       +           +"
+echo "  CCCCCC  R    R       +           +"
+${reset}
