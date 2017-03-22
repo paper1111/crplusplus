d24fd3ec2a68befedc9319a18d3702968440746e
paper1111 <hilarylau119@gmail.com>
2017-03-22 08:05:50 +0800

    Added log

--- installer.sh
+++ installer.sh
@@ -12,9 +12,11 @@ reset=$(tput sgr0)             # Reset
 echo "${boldgreen}Welcome to the Compiler++ installer!${reset}"
 
 ${bold}
-echo "  CCCCCC  RRRRR        +           +"
-echo " CC       R    R       +           +"
-echo "CC        RRRRR    +++++++++   +++++++++"
-echo " CC       R    R       +           +"
-echo "  CCCCCC  R    R       +           +"
+echo "  CCCCCC  RRRRR        ++           ++"
+echo " CC       R    R       ++           ++"
+echo "CC        RRRRR    ++++++++++   ++++++++++"
+echo " CC       R    R       ++           ++"
+echo "  CCCCCC  R    R       ++           ++"
 ${reset}
+
+echo "${boldblue}Installing Homebrew...${reset}
